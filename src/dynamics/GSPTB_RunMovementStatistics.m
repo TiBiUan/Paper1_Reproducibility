f5fb4f76d420684b3c07bed391e0af03627f70e5
TiBiUan <thomas.bolton@epfl.ch>
2026-08-06 23:44:45 +0200

    Delete src directory

--- src/dynamics/GSPTB_RunMovementStatistics.m
+++
@@ -1,8 +0,0 @@
-function [MovementStatistics] = GSPTB_RunMovementStatistics(Data)
-
-    % Establishes movement statistics across tasks for all motion
-    % descriptions we have in the data
-    MovementStatistics.MeanFD = GSPTB_Statistics_MovementAndTask(Data.MeanFD);
-    MovementStatistics.MeanFD_NonScrubbedOnly = GSPTB_Statistics_MovementAndTask(Data.MeanFD_NonScrubbedOnly);
-    MovementStatistics.PercentageScrubbed = GSPTB_Statistics_MovementAndTask(Data.PercentageScrubbed);
-end
